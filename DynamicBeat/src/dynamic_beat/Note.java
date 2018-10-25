be4d4ebd5155799f1837399374d62da4853c86eb
KimSangHyeop <44149229+KimSangHyeop@users.noreply.github.com>
2018-10-25 11:57:47 +0900

    Update Note.java

--- DynamicBeat/src/dynamic_beat/Note.java
+++ DynamicBeat/src/dynamic_beat/Note.java
@@ -8,7 +8,7 @@ import javax.swing.ImageIcon;
 public class Note extends Thread {
 
 	private Image noteBasicIamge = new ImageIcon(Main.class.getResource("../images/noteBasic.png")).getImage();
-	private int x, y = 580 - (1000 / Main.SLEEP_TIME * Main.NOTE_SPEED) * Main.REACH_TIME; // 현제 노트의 위치 알려준다 및 1초 뒤에
+	private int x, y = 580 - (1000 / Main.SLEEP_TIME * Main.NOTE_SPEED) * Main.REACH_TIME; // 現在Noteの位置を教える、そして位置を教える
 																							// 판정라인에 도달하게 한다. y좌표는 달라진다.
 																							// 580은 판정 라인
 	private String noteType;
@@ -23,11 +23,11 @@ public class Note extends Thread {
 	}
 
 	public void close() {
-		proceeded = false;// 노트 이동 불가
+		proceeded = false;//Note移動不可
 	}
 
 	public Note(String noteType) {
-		if (noteType.equals("S")) {// x 값을 동적으로 바꾼다.
+		if (noteType.equals("S")) {// xを動的に変える
 			x = 228;
 		} else if (noteType.equals("D")) {
 			x = 332;
@@ -42,32 +42,32 @@ public class Note extends Thread {
 		} else if (noteType.equals("L")) {
 			x = 952;
 		}
-		this.noteType = noteType; // 초기화
-	}// 현제 노트의 위치를 알려 준다.
+		this.noteType = noteType; // 初期化
+	}// 現在Noteの位置を教える
 
 	public void screenDraw(Graphics2D g) {
-		if (!noteType.equals("Space")) // 스페이스가 아닌 경우에는 일반적인 이미지가 뜨게 한다
+		if (!noteType.equals("Space")) // SpaceKeyがない場合に一般的なNoteのImageを見せる
 		{
 			g.drawImage(noteBasicIamge, x, y, null);
-		} else if (noteType.equals("continuity"))// 연속으로 2개
+		} else if (noteType.equals("continuity"))
 		{
 			g.drawImage(noteBasicIamge, x, y, null);
 			g.drawImage(noteBasicIamge, x, y + 100, null);
-		} else if (noteType.equals("double"))// 연속으로 2개
+		} else if (noteType.equals("double"))
 		{
 			g.drawImage(noteBasicIamge, x, y, null);
 			g.drawImage(noteBasicIamge, x + 105, y, null);
-		} else // space 키 설정
-		{// 스페이스 바에 쓰인다
+		} else // SpaceKey設定
+		{
 			g.drawImage(noteBasicIamge, x, y, null);
 			g.drawImage(noteBasicIamge, x + 100, y, null);
 		}
 	}
 
 	public void drop() {
-		y += Main.NOTE_SPEED;// 아래로 7만큼 떨어진다
+		y += Main.NOTE_SPEED;// Noteが落ちる速さ
 		if (y > 620) {
-			System.out.println("Miss");// 판정 함수
+			System.out.println("Miss");//判定関数
 			close();
 		}
 	}
@@ -78,12 +78,12 @@ public class Note extends Thread {
 			while (true) {
 				drop();
 				if(proceeded) {
-					Thread.sleep(Main.SLEEP_TIME);//쉬는 시간 설정
+					Thread.sleep(Main.SLEEP_TIME);//SleepTime設定
 				}else {
 					interrupt();
 					break;
 				}
-				Thread.sleep(Main.SLEEP_TIME); // 1초에 100번 실행하게 한다
+				Thread.sleep(Main.SLEEP_TIME);
 			}
 			
 		}catch(Exception e) {
@@ -91,7 +91,7 @@ public class Note extends Thread {
 			
 		}
 	
-	}// Thread 실행 함수
+	}
 	public void judge() {
 		if(y >=613) {
 			System.out.println("Late");
