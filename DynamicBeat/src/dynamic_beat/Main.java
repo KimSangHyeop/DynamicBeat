0e19c50236220f145b8faa85c00419bdf75c701f
KimSangHyeop <44149229+KimSangHyeop@users.noreply.github.com>
2018-10-25 11:48:00 +0900

    Update Main.java

--- DynamicBeat/src/dynamic_beat/Main.java
+++ DynamicBeat/src/dynamic_beat/Main.java
@@ -5,8 +5,8 @@ public class Main {
 	public static final int SCREEN_WIDTH = 1280;
 	public static final int SCREEN_HEIGHT = 720;
 	public static final int NOTE_SPEED = 5;//Noteの速さ
-	public static final int SLEEP_TIME = 5;//얼마간의 시간을 주기로 떨어진다.
-	public static final int REACH_TIME = 1;//노트가 생성되고 나서 판정바에 도달하기 까지의 시간 
+	public static final int SLEEP_TIME = 5;//Noteが落ちる時間
+	public static final int REACH_TIME = 1;//Noteが生成されて判定Barに到達するまでの時間 
 	
 	public static void main(String[] args) {
 		
